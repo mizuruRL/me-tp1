56f380811f69c5f031f814e9c2ee42e6dbd1d34a
mizuruRL <andrecfdias42@gmail.com>
2022-04-09 16:23:51 +0100

    boxplots and summaries

--- worksheet.R
+++ worksheet.R
@@ -13,13 +13,15 @@ ageAbs <- table(ageGroups)
 agencyAbs <- table(travel_insurance$Agency)
 destinationAbs <- table(travel_insurance$Destination)
 productAbs <-  table(travel_insurance$Product.Name)
+claimAbs <- table(travel_insurance$Claim)
 
 netRel <- prop.table(netAbs)
 commRel <- prop.table(commAbs)
 ageRel <- prop.table(ageAbs)
 agencyRel <- prop.table(agencyAbs)
 destinationRel <- prop.table(destinationAbs)
-productRel <- prop.table(productNameAbs)
+productRel <- prop.table(productAbs)
+claimRel <- prop.table(claimAbs)
 
 net <- cbind("Frequência Absoluta" = netAbs, "Frequência Relativa" = netRel, "Frequência Relativa %" = netRel*100, "Frequência Abs. Acumulada" = cumsum(netRel), "Frequência Rel.% Acumulada" = cumsum(netRel*100))
 comm <- cbind("Frequência Absoluta" = commAbs, "Frequência Relativa" = commRel, "Frequência Relativa %" = commRel*100, "Frequência Abs. Acumulada" = cumsum(commRel), "Frequência Rel.% Acumulada" = cumsum(commRel*100))
@@ -27,6 +29,7 @@ age <- cbind("Frequência Absoluta" = ageAbs, "Frequência Relativa" = ageRel, "
 agency <- cbind("Frequência Absoluta" = agencyAbs, "Frequência Relativa" = agencyRel, "Frequência Relativa %" = agencyRel*100, "Frequência Abs. Acumulada" = cumsum(agencyRel), "Frequência Rel.% Acumulada" = cumsum(agencyRel*100))
 destination <- cbind("Frequência Absoluta" = destinationAbs, "Frequência Relativa" = destinationRel, "Frequência Relativa %" = destinationRel*100, "Frequência Abs. Acumulada" = cumsum(destinationRel), "Frequência Rel.% Acumulada" = cumsum(destinationRel*100))
 product <- cbind("Frequência Absoluta" = productAbs, "Frequência Relativa" = productRel, "Frequência Relativa %" = productRel*100, "Frequência Abs. Acumulada" = cumsum(productRel), "Frequência Rel.% Acumulada" = cumsum(productRel*100))
+claim <- cbind("Frequência Absoluta" = claimAbs, "Frequência Relativa" = claimRel, "Frequência Relativa %" = claimRel*100, "Frequência Abs. Acumulada" = cumsum(claimRel), "Frequência Rel.% Acumulada" = cumsum(claimRel*100))
 
 View(net)
 View(comm)
@@ -34,3 +37,44 @@ View(age)
 View(agency)
 View(destination)
 View(product)
+View(claim)
+
+summary(travel_insurance$Net.Sales)
+summary(travel_insurance$Commision..in.value.)
+summary(travel_insurance$Agency)
+summary(travel_insurance$Destination)
+summary(travel_insurance$Product.Name)
+summary(travel_insurance$Claim)
+
+
+boxplot(travel_insurance$Age,
+        outline = FALSE,
+        main = "Diagrama de extremos e quartis Idade",
+        ylab = "Idade",
+        col = c("#c8ade6")
+)
+
+boxplot(travel_insurance$Age ~ travel_insurance$Claim,
+        outline = FALSE,
+        main = "Diagrama de extremos e quartis Idade/Seguro Acionado",
+        ylab = "Idade",
+        xlab = "Seguro Acionado",
+        col = c("#c8ade6")
+)
+
+boxplot(travel_insurance$Net.Sales ~ travel_insurance$Agency,
+        outline = FALSE,
+        main = "Diagrama de extremos e quartis Vendas Liquidas/Agência",
+        ylab = "Vendas Líquidas",
+        xlab = "Agências",
+        col = c("#c8ade6"),
+        las = 2
+)
+
+boxplot(travel_insurance$Net.Sales ~ travel_insurance$Claim,
+        outline = FALSE,
+        main = "Diagrama de extremos e quartis Idade",
+        ylab = "Vendas Líquidas",
+        xlab = "Seguro Acionado",
+        col = c("#c8ade6"),
+)
