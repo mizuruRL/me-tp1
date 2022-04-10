956e295a31aae438af1a988a55f22175680ef889
mizuruRL <andrecfdias42@gmail.com>
2022-04-10 03:34:45 +0100

    piechart and table fix

--- worksheet.R
+++ worksheet.R
@@ -17,6 +17,11 @@ destinationAbs <- table(travel_insurance$Destination)
 productAbs <-  table(travel_insurance$Product.Name)
 claimAbs <- table(travel_insurance$Claim)
 
+subset(destinationAbs, destinationAbs > 100)
+
+print(destinationAbs)
+
+
 netRel <- prop.table(netAbs)
 commRel <- prop.table(commAbs)
 ageRel <- prop.table(ageAbs)
@@ -25,13 +30,15 @@ destinationRel <- prop.table(destinationAbs)
 productRel <- prop.table(productAbs)
 claimRel <- prop.table(claimAbs)
 
-net <- cbind("Frequência Absoluta" = netAbs, "Frequência Relativa" = netRel, "Frequência Relativa %" = netRel*100, "Frequência Abs. Acumulada" = cumsum(netRel), "Frequência Rel.% Acumulada" = cumsum(netRel*100))
-comm <- cbind("Frequência Absoluta" = commAbs, "Frequência Relativa" = commRel, "Frequência Relativa %" = commRel*100, "Frequência Abs. Acumulada" = cumsum(commRel), "Frequência Rel.% Acumulada" = cumsum(commRel*100))
-age <- cbind("Frequência Absoluta" = ageAbs, "Frequência Relativa" = ageRel, "Frequência Relativa %" = ageRel*100, "Frequência Abs. Acumulada" = cumsum(ageRel), "Frequência Rel.% Acumulada" = cumsum(ageRel*100))
-agency <- cbind("Frequência Absoluta" = agencyAbs, "Frequência Relativa" = agencyRel, "Frequência Relativa %" = agencyRel*100, "Frequência Abs. Acumulada" = cumsum(agencyRel), "Frequência Rel.% Acumulada" = cumsum(agencyRel*100))
-destination <- cbind("Frequência Absoluta" = destinationAbs, "Frequência Relativa" = destinationRel, "Frequência Relativa %" = destinationRel*100, "Frequência Abs. Acumulada" = cumsum(destinationRel), "Frequência Rel.% Acumulada" = cumsum(destinationRel*100))
-product <- cbind("Frequência Absoluta" = productAbs, "Frequência Relativa" = productRel, "Frequência Relativa %" = productRel*100, "Frequência Abs. Acumulada" = cumsum(productRel), "Frequência Rel.% Acumulada" = cumsum(productRel*100))
-claim <- cbind("Frequência Absoluta" = claimAbs, "Frequência Relativa" = claimRel, "Frequência Relativa %" = claimRel*100, "Frequência Abs. Acumulada" = cumsum(claimRel), "Frequência Rel.% Acumulada" = cumsum(claimRel*100))
+colnames(destination)
+
+net <- cbind("Frequência Absoluta" = netAbs, "Frequência Relativa" = netRel, "Frequência Relativa %" = netRel*100, "Frequência Abs. Acumulada" = cumsum(netAbs), "Frequência Rel.% Acumulada" = cumsum(netRel*100))
+comm <- cbind("Frequência Absoluta" = commAbs, "Frequência Relativa" = commRel, "Frequência Relativa %" = commRel*100, "Frequência Abs. Acumulada" = cumsum(commAbs), "Frequência Rel.% Acumulada" = cumsum(commRel*100))
+age <- cbind("Frequência Absoluta" = ageAbs, "Frequência Relativa" = ageRel, "Frequência Relativa %" = ageRel*100, "Frequência Abs. Acumulada" = cumsum(ageAbs), "Frequência Rel.% Acumulada" = cumsum(ageRel*100))
+agency <- cbind("Frequência Absoluta" = agencyAbs, "Frequência Relativa" = agencyRel, "Frequência Relativa %" = agencyRel*100, "Frequência Abs. Acumulada" = cumsum(agencyAbs), "Frequência Rel.% Acumulada" = cumsum(agencyRel*100))
+destination <- cbind("Frequência Absoluta" = destinationAbs, "Frequência Relativa" = destinationRel, "Frequência Relativa %" = destinationRel*100, "Frequência Abs. Acumulada" = cumsum(destinationAbs), "Frequência Rel.% Acumulada" = cumsum(destinationRel*100))
+product <- cbind("Frequência Absoluta" = productAbs, "Frequência Relativa" = productRel, "Frequência Relativa %" = productRel*100, "Frequência Abs. Acumulada" = cumsum(productAbs), "Frequência Rel.% Acumulada" = cumsum(productRel*100))
+claim <- cbind("Frequência Absoluta" = claimAbs, "Frequência Relativa" = claimRel, "Frequência Relativa %" = claimRel*100, "Frequência Abs. Acumulada" = cumsum(claimAbs), "Frequência Rel.% Acumulada" = cumsum(claimRel*100))
 
 View(net)
 View(comm)
@@ -125,10 +132,23 @@ hist(travel_insurance$Net.Sales,
 
 default_margins <- par("mar")
 margins <- default_margins + c(11.5, 0, 0, 0)
-par(mar = margins) 
+par(mar = margins)
+
 barplot(productAbs,
         main = "Gráfico de barras total de Produtos",
         ylab = "Total de Produtos",
         las = 2,
         col = c("#c8ade6")
 )
+
+par(mar = default_margins)
+
+pieRel <- subset(destinationRel, destinationRel > 0.035)
+
+pie(pieRel,
+        labels = paste0(round(100 * pieRel, 2), "%"),
+        main = "10 destinos mais frequentes",
+        clockwise = TRUE,
+        col = rainbow(10)
+)
+legend("bottomleft", legend = c("Australia", "China", "Hong Kong", "India", "Indonesia", "Malaysia", "Philippines", "Singapore", "Thailand", "United States"), fill = rainbow(10))
